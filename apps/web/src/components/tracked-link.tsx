fa9155e53c0da429b6080db1fd1e4c90749022dc
Kent <kent.fenwick@gmail.com>
2026-05-26 11:34:47 -0400

    fix(web): forward refs from tracked links

--- apps/web/src/components/tracked-link.tsx
+++ apps/web/src/components/tracked-link.tsx
@@ -1,24 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import type { ComponentProps, MouseEvent } from "react";
+import { forwardRef, type ComponentPropsWithoutRef, type MouseEvent } from "react";
 import { captureWebEvent, type AnalyticsProperties } from "@/lib/analytics";
 
-type TrackedLinkProps = ComponentProps<typeof Link> & {
+type TrackedLinkProps = ComponentPropsWithoutRef<typeof Link> & {
   analyticsEvent: string;
   analyticsProperties?: AnalyticsProperties;
 };
 
-export function TrackedLink({
-  analyticsEvent,
-  analyticsProperties,
-  onClick,
-  ...props
-}: TrackedLinkProps) {
+export const TrackedLink = forwardRef<HTMLAnchorElement, TrackedLinkProps>(function TrackedLink(
+  { analyticsEvent, analyticsProperties, onClick, ...props },
+  ref
+) {
   const handleClick = (event: MouseEvent<HTMLAnchorElement>) => {
     captureWebEvent(analyticsEvent, analyticsProperties);
     onClick?.(event);
   };
 
-  return <Link {...props} onClick={handleClick} />;
-}
+  return <Link ref={ref} {...props} onClick={handleClick} />;
+});
